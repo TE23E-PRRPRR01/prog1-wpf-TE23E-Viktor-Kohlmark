a8f7d8a260b2edfe6688c9dee2cb18cf7508c264
Viktor Köhlmark <viktor.kohlmark@elev.ga.ntig.se>
2025-03-03 14:44:30 +0100

    Gjort eppostappen för att sicka epost via en app

---
+++ EPOSTAPP/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Windows;
+using System.Net.Mail;
+using System.Net;
+using System.Data.Common;
+namespace EPOSTAPP;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickSkicka(object sender, RoutedEventArgs e)
+    {
+        string epost = tbxEpost.Text;
+        string meddelande = tbxMeddelande.Text; 
+
+        SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+        smtp.EnableSsl = true; 
+        smtp.Credentials = new NetworkCredential("user", "pass");
+
+        if (epost != "" && meddelande != "")
+        {
+            smtp.Send("komsadef@gmail.com", "epost", "Epost från appen", meddelande ); 
+            lblStatus.Content="✅ ";
+
+        }
+        else
+        {
+            lblStatus.Content="❌ ";
+        }
+    }
+}
